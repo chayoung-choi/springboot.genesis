65fbfb025e315f54a61f87e06fa41afcdcc0775d
chayoung_DK <chayoung.dev@gmail.com>
2020-09-18 09:41:32 +0900

    Add : 회원 웹 기능 등록, 리스트

--- src/main/java/com/cyoung90/genesis/controller/MemberController.java
+++ src/main/java/com/cyoung90/genesis/controller/MemberController.java
@@ -1,20 +1,44 @@
 package com.cyoung90.genesis.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cyoung90.genesis.domain.Member;
 import com.cyoung90.genesis.service.MemberService;
 
 @Controller
 public class MemberController {
-	
-	
-	private final MemberService memeberSerive;
+
+	private final MemberService memberService;
 
 	@Autowired
-	public MemberController(MemberService memeberSerive) {
-		this.memeberSerive = memeberSerive;
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
 	}
-	
-	
+
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping(value = "/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+		memberService.join(member);
+		return "redirect:/";
+	}
+
+	@GetMapping(value = "/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+	}
+
 }
