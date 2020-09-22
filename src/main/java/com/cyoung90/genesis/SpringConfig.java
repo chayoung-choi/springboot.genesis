38757ca095acbcee032bb68991650e11b39b1b5c
chayoung-choi <47589936+chayoung-choi@users.noreply.github.com>
2020-09-22 12:32:57 +0900

    Add : DataSource Connection

--- src/main/java/com/cyoung90/genesis/SpringConfig.java
+++ src/main/java/com/cyoung90/genesis/SpringConfig.java
@@ -1,14 +1,23 @@
 package com.cyoung90.genesis;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.cyoung90.genesis.repository.JdbcMemberRepository;
 import com.cyoung90.genesis.repository.MemberRepository;
-import com.cyoung90.genesis.repository.MemoryMemberRepository;
 import com.cyoung90.genesis.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+	
+	private DataSource dataSource;
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +26,7 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+//		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
