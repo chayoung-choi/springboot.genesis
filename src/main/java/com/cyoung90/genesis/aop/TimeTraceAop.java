f78834b67bf738be5384e557cd4baa96e9da81d5
chayoung-choi <47589936+chayoung-choi@users.noreply.github.com>
2020-09-28 23:06:38 +0900

    Add : AOP Component

---
+++ src/main/java/com/cyoung90/genesis/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package com.cyoung90.genesis.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+	
+	@Around("execution(* com.cyoung90.genesis..*(..))")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		System.out.println("START: " + joinPoint.toString());
+		try {
+			return joinPoint.proceed();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+		}
+	}
+}
